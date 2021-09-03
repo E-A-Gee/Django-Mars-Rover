7fdf6eb749f5f04457d853c02b776c422c040820
E-A-Gee <geelizikers@gmail.com>
2021-09-03 15:39:23 -0400

    complete structure of app

--- rover_app/urls.py
+++ rover_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index-view'),
+    path('results/', views.results, name='results-view'),
+    path('<photo_date>/<photo_id>', views.photo, name='photo-view'),
 ]
