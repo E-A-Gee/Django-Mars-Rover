669c886774ae2349e61790a0eb0ad3f8dc17058d
E-A-Gee <70771300+E-A-Gee@users.noreply.github.com>
2022-11-02 21:39:55 -0400

    Update Settings with Railway Host

--- rover_project/settings.py
+++ rover_project/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('ROVER_SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('ROVER_DEBUG_VALUE') == 'True')
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['geeliz-mars-rover-app.herokuapp.com']
+ALLOWED_HOSTS = ['geeliz-mars-rover.up.railway.app']
 
 
 # Application definition
